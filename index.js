d4c83f8e7d08af0ab322a17d2a9403405bb5e4ad
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-11 09:10:22 +0300

    setup nodemon and create app.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const app = express();
+const app = require('./app');
 const PORT = process.env.port || 5000;
-
-app.get('/',(req,res) => {
-   res.status(200).json({
-       message: "Working"
-   })
-});
 
 app.listen(PORT, () => console.log(`server has been started at ${PORT}`)
 );
