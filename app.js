cbb4c285d50c8ab4cd22088687beb5beb7302df5
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-11 10:04:07 +0300

    bodyParser urlencoded

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
@@ -7,7 +8,8 @@ const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use('/api/auth',authRoutes);
 app.use('/api/analytics',analyticsRoutes);
