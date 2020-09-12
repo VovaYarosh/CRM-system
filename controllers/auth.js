a2089e82c3a97e836c17a183854c1c96909f19a4
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-12 07:47:49 +0300

    start making register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const  User = require('../models/User');
+
 module.exports.login = function (req,res) {
     res.status(200).json({
         login: {
@@ -7,7 +9,9 @@ module.exports.login = function (req,res) {
     })
 };
 module.exports.register = function (req,res) {
-    res.status(200).json({
-        register: true
-    })
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save().then(()=> console.log('user created'));
 };
