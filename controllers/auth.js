d94814b060da7c7520d94997ab0c9c92c867ad0e
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-12 08:09:05 +0300

    register check if email already exists

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,10 +8,14 @@ module.exports.login = function (req,res) {
         }
     })
 };
-module.exports.register = function (req,res) {
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
+module.exports.register = async function (req,res) {
+    const candidate = await User.findOne({email: req.body.email});
+    if(candidate){
+        res.status(409).json({
+            message: 'this email already exists?try another one'
+        })
+    }else{
+
+    }
     user.save().then(()=> console.log('user created'));
 };
