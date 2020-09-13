08f075d86627d89cdd02ba76b486f1fd0e947a26
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-13 21:38:33 +0300

    order controllers

--- routes/order.js
+++ routes/order.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const controller = require('../controllers/order');
 
 
-router.get('/',controller.getAll);
-router.post('/',controller.register);
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create);
 
 module.exports = router;
--- routes/position.js
+++ routes/position.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const controller = require('../controllers/position');
 
 
-router.get('/:categoryId',controller.getByCategoryId);
+router.get('/:categoryId', passport.authenticate('jwt', {session: false}), controller.getByCategoryId);
 
-router.post('/',controller.create);
+router.post('/', passport.authenticate('jwt', {session: false}), controller.create);
 
-router.patch('/:id',controller.update);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), controller.update);
 
-router.delete('/:id',controller.remove);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), controller.remove);
 
 module.exports = router;
