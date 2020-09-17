86a9606fad422b27682c1b6001bf7a507b0be2bf
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-17 21:24:14 +0300

    position connect PositionService

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -3,6 +3,7 @@ import {PositionsService} from "../../../shared/services/positions.service";
 import {Position} from "../../../interface";
 import {MaterialInstance, MaterialService} from "../../../shared/classes/material.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {error} from "selenium-webdriver";
 
 @Component({
   selector: 'app-positions-form',
@@ -53,6 +54,25 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
     this.modal.close()
   }
   onSubmit(){
+    this.form.disable();
+    const newPosition: Position = {
+      name: this.form.value.name,
+      cost: this.form.value.cost,
+      category: this.categoryId
+    }
+    this.positionsService.create(newPosition).subscribe(
+      position => {
+        MaterialService.toast('position created');
+        this.positions.push(position)
+      },
+      error => {
+        this.form.enable();
+        MaterialService.toast(error.error.message)
+      }
+    )
+  }
+
+  onDeletePosition(position: Position) {
 
   }
 }
