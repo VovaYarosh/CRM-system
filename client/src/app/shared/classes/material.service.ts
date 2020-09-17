910ff02e91469bc1cbc94a146b86a23a6847a709
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-09-17 09:28:59 +0300

    add and edit positions modal window

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -1,6 +1,12 @@
 import {ElementRef} from "@angular/core";
 
-declare var M
+declare var M;
+
+export interface MaterialInstance {
+  open?(): void
+  close?(): void
+  destroy?(): void
+}
 
 export class MaterialService {
   static toast(message: string){
@@ -12,5 +18,8 @@ export class MaterialService {
   }
   static updateTextInputs(){
     M.updateTextFields()
+  }
+  static initModal(ref: ElementRef): MaterialInstance{
+    return M.Modal.init(ref.nativeElement)
   }
 }
---
+++ client/src/app/shared/services/positions.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Position} from "../../interface";
+import {Observable} from "rxjs/internal/Observable";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PositionsService {
+  constructor(private http: HttpClient){
+  }
+  fetch(categoryId: string): Observable<Position[]>{
+    return this.http.get<Position[]>(`/api/position/${categoryId}`)
+  }
+}
